a368dee824bbc4036756d9fd5d56935a463a8775
ricardosf <ricardosepu93@gmail.com>
2022-09-27 00:11:16 -0700

    added hidden class

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ const $decrypt = d.getElementById("decrypt");
 $copy.addEventListener("click", (e) => {
   navigator.clipboard.writeText($output.innerText).then(
     () => {
-      window.alert("text copied to clipboard");
+      d.querySelector(".text-copied").innerText = "Texto copiado!";
+      setTimeout(() => {
+        d.querySelector(".text-copied").innerText = "";
+      }, 1000);
     },
     () => {
       window.alert("Sorry, your browser does not support Clipboard API");
@@ -19,8 +22,7 @@ $copy.addEventListener("click", (e) => {
 });
 
 $encrypt.addEventListener("click", (e) => {
-  const inputText = $input.value.split("");
-  // console.log(inputText);
+  const inputText = $input.value.toLowerCase().split("");
   let salida = "";
 
   inputText.map((el) => {
@@ -44,3 +46,24 @@ $encrypt.addEventListener("click", (e) => {
   $output.innerText = salida;
   $noText.classList.add("is-active");
 });
+
+$decrypt.addEventListener("click", (e) => {
+  let encryptedText = $input.value.toLowerCase();
+  const mapObj = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
+
+  $output.innerText = decryptText(encryptedText, mapObj);
+  $noText.classList.add("is-active");
+});
+
+/* Function applies the replace method to the provided text and search for encrypted words and replace them with the original letter */
+function decryptText(text, mapObj) {
+  const reg = new RegExp(Object.keys(mapObj).join("|"), "g");
+  let result = text.replace(reg, (matched) => mapObj[matched]);
+  return result;
+}
